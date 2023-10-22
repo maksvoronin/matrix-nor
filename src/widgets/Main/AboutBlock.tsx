db3f14008e486b5c86f03ef608261879176821a8
Maxim <catlense@voronin.xyz>
2023-10-22 23:55:37 +0700

    Change avatar background

--- src/widgets/Main/AboutBlock.tsx
+++ src/widgets/Main/AboutBlock.tsx
@@ -2,21 +2,22 @@ import { observer } from "mobx-react";
 import { FC } from "react";
 import styled from "styled-components";
 import { Container } from "../../shared";
-import lilia from "../../assets/lilia_nor.png"
+import lilia from "../../assets/lilia_nor.png";
+import julia from "../../assets/julia_nor.png";
 
 const Title = styled.h1`
   font-weight: 400;
   font-size: 32px;
   margin-bottom: 41px;
   text-align: center;
-  @media(max-width: 500px) {
+  @media (max-width: 500px) {
     font-size: 24px;
   }
 `;
 
 const BlockList = styled.div`
   display: flex;
-  @media(max-width: 1040px) {
+  @media (max-width: 1040px) {
     flex-direction: column;
     gap: 20px;
   }
@@ -31,9 +32,10 @@ const UserCard = styled.div`
     height: 285px;
     display: block;
     border-radius: 8px;
+    background: #3f4052;
   }
 
-  @media(max-width: 500px) {
+  @media (max-width: 500px) {
     flex-direction: column;
     width: 100%;
     align-items: center;
@@ -49,13 +51,17 @@ const Text = styled.div`
     max-width: 261px;
     font-size: 18px;
     line-height: 1.47;
-    @media(max-width: 500px) {
+    @media (max-width: 500px) {
       max-width: none;
     }
   }
-  @media(max-width: 500px) {
-      text-align: center;
-    }
+  @media (max-width: 500px) {
+    text-align: center;
+  }
+`;
+
+const Img = styled.img`
+  width: 240px !important;
 `;
 
 const AboutBlock: FC = observer(() => {
@@ -71,7 +77,7 @@ const AboutBlock: FC = observer(() => {
           </Text>
         </UserCard>
         <UserCard>
-          <img alt="some" />
+          <Img alt="some" src={julia} />
           <Text>
             <h1>Юлия Нор</h1>
             <p>Spiritual Guide, парапсихолог, биоэнерготерапевт, системный расстановщик, причинно-следственный корректор, астронумеролог, гипнолог. Практикующий личный консультант более 17 лет.</p>
