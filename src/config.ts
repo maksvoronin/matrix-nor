68ca339cd5ff2e93f08cfbd9147731149cbc7e1b
Maxim <catlense@voronin.xyz>
2023-10-27 16:28:49 +0700

    change telegram in header

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  telegram_name: "@mv_voronin",
-  telegram_link: "https://t.me/mv_voronin",
+  telegram_name: "@matrixnor",
+  telegram_link: "https://t.me/matrixnor",
   api: "http://matrixnor.voronin.xyz/api/sendmail",
 };
 
